d52bfd280d93b2bf38a849818ab6913d5804e39d
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-09-05 15:25:49 -0500

    (feat) Added new favicon and Signup Component boilerplate

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Nav from './Nav/Nav';
 import Main from './Main/Main';
+import Signup from './Signup/Signup';
 class App extends React.Component {
     render() {
         return (
             <Router>
                 <div className="container">
                     <Nav />
-                    <Main />
+                    <Route exact path="/" component={Main} />
+                    <Route path="/start" component={Signup} />
                 </div>
             </Router>
         )
