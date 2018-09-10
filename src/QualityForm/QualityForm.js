b4144e9015e1ed112372c1296fdef5e87ddb037b
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-09-10 08:57:17 -0500

    (feat) Added text input for exact number entry in Quality form

--- src/QualityForm/QualityForm.js
+++ src/QualityForm/QualityForm.js
@@ -3,11 +3,31 @@ import './qualityform.css';
 
 class QualityForm extends React.Component {
     state = {
-        quality: 5
+        quality: 5,
+        editable: false
     }
 
-    handleClick(event) {
-        this.setState(Object.assign({},this.state.quality,{quality: event.target.value}));
+    handleSliderChange(event) {
+        this.setState(Object.assign({},this.state,{
+            quality: event.target.value,
+            editable: false
+        }));
+    }
+
+    handleRatingClick() {
+        this.setState(Object.assign({},this.state,{editable: true}));
+    }
+
+    handleInputChange(event) {
+        let rating = event.target.value;
+        if(rating !== "") {
+            rating = Math.max(Math.min(rating, 10),0);
+        }
+        this.setState(Object.assign({},this.state,{quality: rating})); 
+    }
+
+    handleLoseFocus() {
+        this.setState(Object.assign({},this.state,{editable: false})); 
     }
 
     render() {
@@ -15,9 +35,27 @@ class QualityForm extends React.Component {
             <div className="qualityform">
                 <form className="qualityform__inputs">
                     <h2>Minimum Rating</h2>
-                    <div className="qualityform__rating">{this.state.quality}</div>
+                    {this.state.editable ?
+                        // TODO: Change this to component so I can use HOC click outside wrapper
+                        <input
+                            className="qualityform__rating-input"
+                            onChange={(e)=>{this.handleInputChange(e)}}
+                            onBlur={()=>this.handleLoseFocus()}
+                            type="number"
+                            min="0"
+                            max="10"
+                            step="0.1"
+                            value={this.state.quality}
+                        /> :
+                        <div
+                            className="qualityform__rating-text"
+                            onClick={()=>{this.handleRatingClick()}}
+                        >
+                            {this.state.quality}
+                        </div>
+                    }
                     <input 
-                        onChange={(e)=>{this.handleClick(e)}} 
+                        onChange={(e)=>{this.handleSliderChange(e)}} 
                         min="0" 
                         max="10" 
                         step="0.1" 
