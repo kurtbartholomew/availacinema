d31f24018a5c2956ff893f13fbf509611b239600
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-09-27 23:38:56 -0500

    (feat) Added Suggestion Service and tests

--- server/services/SuggestionService.js
+++ server/services/SuggestionService.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const db = require('../db');
 const UserFilter = require('../models/UserFilter');
 const Movie = require('../models/Movie');
@@ -8,71 +7,84 @@ const Suggestion = require('../models/Suggestion');
 const EmailMessageService = require('../services/EmailMessageService');
 const moment = require('moment');
 
-
-module.exports = {
-    async getSuggestionsForUser(userId, email, isDaily) {
-        const filters = UserFilter.findByUserId(userId);
-        const { genreFilters, ratingFilter } = extractFilters(filters);
-        const movies = await findMoviesMatchingUserFilters(genreFilters,ratingFilter, isDaily);
-        if(movies && movies.length) {
-            const suggestions = [];
-            for(let movie in movies) {
-                suggestions.push({
-                    title: movie.title,
-                    rating: movie.rating,
-                    userId,
-                    movieId: movie.id
+// TODO: Had to put these helper functions into an
+// object would be reachable by tests. Gotta find a better way to do this.
+const utils = {
+    extractFilters(filters, userId) {
+        let ratingFilter, genreFilters;
+        if(filters && filters.length) {
+            const ratingFilterIdx = filters.findIndex((filter) => {
+                return filter.type === UserFilter.TYPE.RATING
+            });
+            if(ratingFilterIdx !== -1) {
+                ratingFilter = filters[ratingFilterIdx];
+                genreFilters = filters.filter((_, idx)=>{
+                    return idx !== ratingFilterIdx;
                 });
+                if(genreFilters.length === 0) {
+                    throw new Error(`Invalid user: No genre filters exist for user ${userId}`);
+                   // TODO: Delete user and associated filters if invalid filters? 
+                }
+            } else {
+                // TODO: Delete user and associated filters if invalid filters?
+                throw new Error(`Invalid user: No rating filter exists for user ${userId}`);
             }
-            await Suggestion.addAll(suggestions);
-            await EmailMessageService.sendSuggestionsEmail(email, suggestions, isDaily);
+        } else {
+            throw new Error(`Invalid user: No filters exist for user ${userId}`);
         }
+    
+        return {
+            ratingFilter,
+            genreFilters
+        };
+    },
+    async findMoviesMatchingUserFilters(genreFilters, ratingFilter, isDaily) {
+        const genreIds = genreFilters.map((filter)=> filter.value);
+        let date = isDaily ? moment().utc() : moment().utc().subtract(7,'days');
+        date = date.format('YYYY-MM-DD');
+        return db(Movie.TABLE)
+                .distinct()
+                .select(`${Movie.TABLE}.*`)
+                .join(`${MovieGenre.TABLE}`, `${Movie.TABLE}.id`, '=', `${MovieGenre.TABLE}.movie_id`)
+                .join(`${Genre.TABLE}`, `${MovieGenre.TABLE}.genre_id`, '=', `${Genre.TABLE}.id`)
+                .whereIn(`${Genre.TABLE}.tmdb_id`, genreIds)
+                .andWhere(`${Movie.TABLE}.rating`,'>=',ratingFilter.value)
+                .andWhere(`${Movie.TABLE}.release_date`,'>=', date)
+                .orderBy(`${Movie.TABLE}.rating`, 'desc');
+    }
+};
+
+async function getAndSendSuggestionsToUser(userId, email, isDaily) {
+    if(userId === undefined) {
+        throw new Error("Missing user id: Cannot send email to undefined user");
+    }
+    const filters = UserFilter.findByUserId(userId);
+    const { genreFilters, ratingFilter } = utils.extractFilters(filters, userId);
+    const movies = await utils.findMoviesMatchingUserFilters(genreFilters,ratingFilter, isDaily);
+    if(movies && movies.length) {
+        const suggestions = [];
+        for(let movie in movies) {
+            suggestions.push({
+                title: movie.title,
+                rating: movie.rating,
+                userId,
+                movieId: movie.id
+            });
+        }
+        await Suggestion.addAll(suggestions);
+        await EmailMessageService.sendSuggestionsEmail(email, suggestions, isDaily);
     }
 }
 
 // TODO: Figure out a way to reduce coupling
 // The other option was put it in the model but I'm not sure its any
 // better to hardcode so much business logic in the data models
-function findMoviesMatchingUserFilters(genreFilters, ratingFilter, isDaily) {
-    const genreIds = genreFilters.map((filter)=> filter.value);
-    let date = isDaily ? moment().utc() : moment().utc().subtract(7,'days');
-    date = date.format('YYYY-MM-DD');
-    return db(Movie.TABLE)
-            .distinct()
-            .select(`${Movie.TABLE}.*`)
-            .join(`${MovieGenre.TABLE}`, `${Movie.TABLE}.id`, '=', `${MovieGenre.TABLE}.movie_id`)
-            .join(`${Genre.TABLE}`, `${MovieGenre.TABLE}.genre_id`, '=', `${Genre.TABLE}.id`)
-            .whereIn(`${Genre.TABLE}.tmdb_id`, genreIds)
-            .andWhere(`${Movie.TABLE}.rating`,'>=',ratingFilter.value)
-            .andWhere(`${Movie.TABLE}.release_date`,'>=', date)
-            .orderBy(`${Movie.TABLE}.rating`, 'desc');
-}
 
-function extractFilters(filters) {
-    let ratingFilter, genreFilters;
-    if(filters && filters.length) {
-        const ratingFilterIdx = filters.findIndex((filter) => {
-            return filter.type === UserFilter.TYPE.RATING
-        });
-        if(ratingFilterIdx !== -1) {
-            ratingFilter = filters[ratingFilterIdx];
-            genreFilters = filters.filter((_, idx)=>{
-                return idx !== ratingFilterIdx;
-            });
-            if(genreFilters.length === 0) {
-                throw new Error(`Invalid user: No genre filters exist for user ${userId}`);
-               // TODO: Delete user and associated filters if invalid filters? 
-            }
-        } else {
-            // TODO: Delete user and associated filters if invalid filters?
-            throw new Error(`Invalid user: No rating filter exists for user ${userId}`);
-        }
-    } else {
-        throw new Error(`Invalid user: No filters exist for user ${userId}`);
-    }
 
-    return {
-        ratingFilter,
-        genreFilters
-    };
+
+
+module.exports = {
+    getAndSendSuggestionsToUser,
+    // included below for the purpose of being able to mock in tests
+    utils
 }
---
+++ server/tests/services/SuggestionService.spec.js
@@ -0,0 +1,112 @@
+const chai = require('chai');
+const assert = chai.assert;
+const sinon = require('sinon');
+const SuggestionService = require('../../services/SuggestionService');
+const EmailMessageService = require('../../services/EmailMessageService');
+const UserFilter = require('../../models/UserFilter');
+const Suggestion = require('../../models/Suggestion');
+
+
+describe('Suggestion Service', () => {
+    describe('getAndSendSuggestionsToUser', () => {
+        beforeEach(() => {
+            this.sandbox = sinon.createSandbox();
+        });
+    
+        afterEach(() => {
+            this.sandbox.restore();
+        });
+
+        it('should throw an error if not passed a user id', async () => {
+            let error;
+            try {
+                await SuggestionService.getAndSendSuggestionsToUser();
+            } catch(e) {
+                error = e;
+            }
+            assert.isDefined(error);
+            assert.match(error.toString(),/Missing user id/);
+        });
+
+        it('should throw an error if no filters are retrieved', async () => {
+            this.sandbox.replace(UserFilter, 'findByUserId', function () { return [] });
+            let error;
+            try {
+                await SuggestionService.getAndSendSuggestionsToUser(72,'someone@gmail.com',true);
+            } catch(e) {
+                error = e;
+            }
+            assert.isDefined(error);
+            assert.match(error, /No filters exist/);
+        });
+
+        it('should throw an error if lacking a rating filter', async () => {
+            this.sandbox.replace(UserFilter, 'findByUserId', () => [
+                {type:UserFilter.TYPE.GENRE,value:43}
+            ]);
+            let error;
+            try {
+                await SuggestionService.getAndSendSuggestionsToUser(72,'someone@gmail.com',true);
+            } catch(e) {
+                error = e;
+            }
+            assert.isDefined(error);
+            assert.match(error, /No rating filter exists/);
+        });
+
+        it('should throw an error if lacking genre filters', async () => {
+            this.sandbox.replace(UserFilter, 'findByUserId', () => [
+                { type: UserFilter.TYPE.RATING, value: 4.3 }
+            ]);
+            let error;
+            try {
+                await SuggestionService.getAndSendSuggestionsToUser(72,'someone@gmail.com',true);
+            } catch(e) {
+                error = e;
+            }
+            assert.isDefined(error);
+            assert.match(error, /No genre filters exist/);
+        });
+
+        it('should not send movie suggestions email if no matching movies found', async () => {
+            this.sandbox.replace(UserFilter, 'findByUserId', () => [
+                { type: UserFilter.TYPE.RATING, value: 4.3 },
+                { type: UserFilter.TYPE.GENRE, value: 79384 }
+            ]);
+            const findMoviesMatchingUserFiltersFake = sinon.fake.returns([]);
+            this.sandbox.replace(SuggestionService.utils, 'findMoviesMatchingUserFilters', findMoviesMatchingUserFiltersFake);
+            const addAllFake = sinon.fake.returns(true);
+            this.sandbox.replace(Suggestion, 'addAll', addAllFake);
+            const sendSuggestionsEmailFake = sinon.fake.returns(true);
+            this.sandbox.replace(EmailMessageService, 'sendSuggestionsEmail', sendSuggestionsEmailFake);
+
+            await SuggestionService.getAndSendSuggestionsToUser(72,'someone@gmail.com',true);
+
+            assert.equal(addAllFake.callCount, 0);
+            assert.equal(sendSuggestionsEmailFake.callCount, 0);
+        });
+
+        
+
+        it('should attempt to send a movie suggestions email if matches found', async () => {
+            this.sandbox.replace(UserFilter, 'findByUserId', () => [
+                { type: UserFilter.TYPE.RATING, value: 4.3 },
+                { type: UserFilter.TYPE.GENRE, value: 79384 }
+            ]);
+            const findMoviesMatchingUserFiltersFake = sinon.fake.returns([
+                {title:"Dracula", rating: 8.9, id: 73452},
+                {title:"Honey, I Shrunk The Kids", rating: 3.5, id: 9243}
+            ]);
+            this.sandbox.replace(SuggestionService.utils, 'findMoviesMatchingUserFilters', findMoviesMatchingUserFiltersFake);
+            const addAllFake = sinon.fake.returns(true);
+            this.sandbox.replace(Suggestion, 'addAll', addAllFake);
+            const sendSuggestionsEmailFake = sinon.fake.returns(true);
+            this.sandbox.replace(EmailMessageService, 'sendSuggestionsEmail', sendSuggestionsEmailFake);
+
+            await SuggestionService.getAndSendSuggestionsToUser(72,'someone@gmail.com',true);
+
+            assert.equal(addAllFake.callCount, 1);
+            assert.equal(sendSuggestionsEmailFake.callCount, 1);
+        });
+    });
+});
