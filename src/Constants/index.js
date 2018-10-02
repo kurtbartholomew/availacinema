88f2a1615bfaf529ebcd45ac4fa3f505a69b856f
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-10-02 13:14:37 -0500

    (fix) Correctly uppercased Constants folder

---
+++ src/Constants/index.js
@@ -0,0 +1,17 @@
+const PANEL_STATE = {
+    UNTOUCHED: 0,
+    INVALID: 1,
+    VALID: 2
+};
+
+const CONTACT_OPTIONS = {
+    EMAIL_DAILY: 0,
+    EMAIL_WEEKLY: 1,
+    TEXT_DAILY: 2,
+    TEXT_WEEKLY: 3
+}
+
+export {
+    PANEL_STATE,
+    CONTACT_OPTIONS
+}
